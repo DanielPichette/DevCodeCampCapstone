936f827f06c301df072c5632ddb27225bfadfc01
DanielPichette <daniel.f.pichette@gmail.com>
2021-07-03 18:16:39 -0500

    added Item User Model

--- capstoneproject/capstoneapp/models.py
+++ capstoneproject/capstoneapp/models.py
@@ -33,3 +33,19 @@ class ItemListing(models.Model):
 
     def __int__(self):
         return self.name
+
+
+class User(models.Model):
+    firstName = models.CharField(max_length=50)
+    lastName = models.CharField(max_length=50)
+    username = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
+    zipcode = models.CharField(max_length=50)
+    phone = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+
+    def __int__(self):
+        return self.username
